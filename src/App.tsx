60b1e672eeecb0d9ae8593d3c26f26434df32e90
Vladislav <mngr@feip.co>
2022-10-27 16:33:51 +1000

    Love

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import clsx from 'clsx';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [dots] = useState([
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10
+    ]);
+
+    return (
+        <div className="App">
+            <header>
+                <h2 className="EmojiGroup">
+                    <span>
+                        👃🏻
+                    </span>
+                    <span>
+                        🐗️
+                    </span>
+                    <span>
+                        ⭐
+                    </span>
+                </h2>
+            </header>
+
+            <footer>
+                <div className="Line"></div>
+                <div className="Dots">
+                    {dots.map((dot) => (
+                        <div className={clsx('Dot', dot === 3 && 'CurrentDot')} data-dot={dot}>
+                            <div className="DotContent">
+                                {dot === 10 ? '∞' : dot === 3 ? (
+                                    <>
+                                        <span>{dot}</span>
+                                        <span>года</span>
+                                    </>
+                                ) : dot}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </footer>
+
+            <div className="heart">
+                <h1>
+                    <span>
+                        ❤️
+                    </span>
+                    <span>
+                        🖤
+                    </span>
+                    <span>
+                        💕
+                    </span>
+                </h1>
+            </div>
+
+            <main>
+                <p>Приглашаю тебя на празднование <b>трехлетия</b> отношений!</p>
+                <p>Я вызову тебе такси в неизвестном направлении - просто жди!</p>
+                <p>Я тебя очень люблю и обожаю!</p>
+            </main>
+        </div>
+    );
 }
 
 export default App;
